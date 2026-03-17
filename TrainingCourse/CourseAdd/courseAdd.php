66860cfa923fe3fe3aeca80c5a99ab6a5e34b888
Jeffrey <jeffriihesinulo@gmail.com>
2026-03-17 02:34:03 +0000

    Add Training course

---
+++ TrainingCourse/CourseAdd/courseAdd.php
@@ -0,0 +1,43 @@
+<?php
+/*
+     * Name: Dzheffriei Iheisnulo
+     * Student ID: C00311856
+     * Date: 17.02.2026
+     */
+
+/** @var mysqli $con */
+require_once __DIR__ . '/../../db.inc.php';//connection to db necessary for phpstorm
+ini_set('display_errors', 1); //display error
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$name = $_POST['addName']; //creating variables and passing data from html fields
+$address = $_POST['addAddress'];
+$eircode = $_POST['addEircode'];
+$phone = $_POST['addPhone'];
+$website = $_POST['addWebsite'];
+$description = $_POST['addDescription'];
+$contactName = $_POST['addContactName'];
+$contactPhone = $_POST['addContactPhone'];
+$contactEmail = $_POST['addContactEmail'];
+
+//sql query for insert
+$sql = "INSERT INTO Company (company_name, address, eircode, telephone, website, business_description, contact_name, contact_phone, contact_email)
+         VALUES('$name', '$address', '$eircode', '$phone', '$website', '$description', '$contactName', '$contactPhone', '$contactEmail')";
+
+//if connection failed or sql query is wrong
+if(!mysqli_query($con, $sql))
+{
+    die("Ann Error in the SQL Query ". mysqli_error($con));
+}
+//else
+echo "<br>A record has been added for   " .
+    $name;
+//closing connection
+mysqli_close($con);
+?>
+<!--Button for return back-->
+<form action="../companyPage.html" method="POST">
+    <br>
+    <input type="submit" value="Return to Company Page"/>
+</form>
---
+++ TrainingCourse/CourseDelete/deleteCourseListbox.php
@@ -0,0 +1,42 @@
+<?php
+/*
+     * Name: Dzheffriei Iheisnulo
+     * Student ID: C00311856
+     * Date: 05.02.2026
+     */
+/** @var mysqli $con */ //connection to db necessary for phpstorm
+require_once __DIR__ . '/../../db.inc.php';
+ini_set('display_errors', 1); //display error
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$sql = "SELECT company_id, company_name, address, eircode, telephone, website, business_description, contact_name, contact_phone, contact_email FROM Company where is_deleted = 0"; //querying the db
+
+if (!$result = mysqli_query($con, $sql)) //checking db connection
+{
+    die('Error in querying the database' . mysqli_error($con));
+}
+
+echo "<select name = 'deleteCompanyListbox' id = 'deleteCompanyListbox' onclick = 'populate()'>"; //setting id to 'deleteCompanyListbox' and setting the onclick to populate function
+
+while($row = mysqli_fetch_array($result)) //setting the data to variables
+{
+    $companyId = $row['company_id'];
+    $companyName = $row['company_name'];
+    $companyAddress = $row['address'];
+    $companyEircode = $row['eircode'];
+    $companyPhone = $row['telephone'];
+    $companyWebsite = $row['website'];
+    $companyBusinessDescription = $row['business_description'];
+    $companyContactName = $row['contact_name'];
+    $companyContactPhone = $row['contact_phone'];
+    $companyContactEmail = $row['contact_email'];
+
+    $allText = "$companyId|$companyName|$companyAddress|$companyEircode|$companyPhone|$companyWebsite|$companyBusinessDescription|$companyContactName|$companyContactPhone|$companyContactEmail";
+    //$allText is now filled with data splited by '|'
+    echo "<option value = '$allText'>$companyName</option>";
+    //option is now saved and the user can see the company name in Listbox that will store all the company data
+}
+echo"</select>";
+mysqli_close($con);
+?>
